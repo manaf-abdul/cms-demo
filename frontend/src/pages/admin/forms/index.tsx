8b88934ecb4d529f0a9ccd11268c7a893a8490a1
manaf-abdul <abdulmanafp1996@gmail.com>
2024-03-25 00:07:38 +0530

    FIX:EDIT FLOW

--- frontend/src/pages/admin/forms/index.tsx
+++ frontend/src/pages/admin/forms/index.tsx
@@ -80,58 +80,63 @@ const Forms = () => {
         setTheme({ colorCode: newValue });
     }
     return (
-        <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" gap={2}>
-            <Typography>
-                Drag and Drop to rearrange
-            </Typography>
-            {!loading && <> <Typography>
-                Select theme
-            </Typography>
-                <MuiColorInput format="hex" value={theme.colorCode} onChange={handleChange} />
-            </>
+        <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" gap={3}>
+            {!loading &&
+                <div>
+                    <Typography>
+                        Select theme
+                    </Typography>
+                    <MuiColorInput format="hex" value={theme.colorCode} onChange={handleChange} />
+                </div>
             }
             <div className="sortable-list">
+
                 {loading ? <CircularProgressBar /> :
-                    <List dense={true} sx={{ fontSize: "5rem" }}>
-                        {forms?.map((item: any, index: any) => (
-                            <div
-                                key={item._id}
-                                className=
-                                {`item ${item === draggingItem ?
-                                    'dragging' : ''
-                                    }`}
-                                draggable="true"
-                                onDragStart={(e) =>
-                                    handleDragStart(e, item)}
-                                onDragEnd={handleDragEnd}
-                                onDragOver={handleDragOver}
-                                onDrop={(e) => handleDrop(e, item)}
-                            >
-                                <div className="details">
-                                    {/*   */}
-                                    <ListItem key={index}
-                                        sx={{
-                                            // border: 1,
-                                            borderRadius: "1rem",
-                                            marginTop: 1,
-                                            cursor: "grab",
-                                            boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.4)", // Add box shadow
-                                            '&:hover': {
-                                                boxShadow: "0px 8px 16px rgba(0, 0, 0, 0.5)", // Increase box shadow on hover
-                                            },
-                                        }} className='px-16 py-2' onClick={() => navigateHandler(item._id)}>
-                                        <ListItemIcon>
-                                            <FolderIcon sx={{ fontSize: "2rem" }} />
-                                        </ListItemIcon>
-                                        <ListItemText
-                                            primary={<span style={{ fontSize: "1.5rem" }}>{item?.formName}</span>}
-                                            secondary={`${item?.formFields?.length} Fields`}
-                                        />
-                                    </ListItem>
+                    <>
+                        <Typography textAlign="center">
+                            Drag and Drop to rearrange
+                        </Typography>
+                        <List dense={true} sx={{ fontSize: "5rem" }}>
+                            {forms?.map((item: any, index: any) => (
+                                <div
+                                    key={item._id}
+                                    className=
+                                    {`item ${item === draggingItem ?
+                                        'dragging' : ''
+                                        }`}
+                                    draggable="true"
+                                    onDragStart={(e) =>
+                                        handleDragStart(e, item)}
+                                    onDragEnd={handleDragEnd}
+                                    onDragOver={handleDragOver}
+                                    onDrop={(e) => handleDrop(e, item)}
+                                >
+                                    <div className="details">
+                                        {/*   */}
+                                        <ListItem key={index}
+                                            sx={{
+                                                // border: 1,
+                                                borderRadius: "1rem",
+                                                marginTop: 1,
+                                                cursor: "grab",
+                                                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.4)", // Add box shadow
+                                                '&:hover': {
+                                                    boxShadow: "0px 8px 16px rgba(0, 0, 0, 0.5)", // Increase box shadow on hover
+                                                },
+                                            }} className='px-16 py-2' onClick={() => navigateHandler(item._id)}>
+                                            <ListItemIcon>
+                                                <FolderIcon sx={{ fontSize: "2rem" }} />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary={<span style={{ fontSize: "1.5rem" }}>{item?.formName}</span>}
+                                                secondary={`${item?.formFields?.length} Fields`}
+                                            />
+                                        </ListItem>
+                                    </div>
                                 </div>
-                            </div>
-                        ))}
-                    </List>
+                            ))}
+                        </List>
+                    </>
                 }
             </div>
         </Box>
