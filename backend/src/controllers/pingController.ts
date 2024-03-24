25d767a8b38f76fddf32fa4675b8588357757976
manaf-abdul <abdulmanafp1996@gmail.com>
2024-03-25 00:34:26 +0530

    DEPLOY BE

--- backend/src/controllers/pingController.ts
+++ backend/src/controllers/pingController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express"
 
 export const ping = async (req: Request, res: Response) => {
-    const message = "Welcom to nearby vendors heyyyy"
+    const message = "Welcom to APIs"
     res.status(200).json(message)
 }
